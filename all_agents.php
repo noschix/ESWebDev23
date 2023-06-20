4602ab75ddecfc1af6cae35913353c98a96da5e2
Noah Raissi <coding@noahraissi.com>
2023-06-20 15:25:02 +0200

    new approach

---
+++ all_agents.php
@@ -0,0 +1,26 @@
+<?php
+// Assuming you have a database connection established
+include('db.php'); 
+header('Content-Type: application/json');
+// Fetch all agent data from the database
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "SELECT * FROM agents";
+$result = $conn->query($sql);
+$data = array();
+
+if ($result->num_rows > 0) {
+    // Output data of each row
+    while($row = $result->fetch_assoc()) {
+        $data[] = $row;
+    }
+} else {
+    echo "0 results";
+}
+echo json_encode($data);
+
+$conn->close();
+?>
