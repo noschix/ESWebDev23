82848a27099d30f7c5c13a9a029bb686b24e7eb1
Noah Raissi <coding@noahraissi.com>
2023-06-20 10:17:11 +0200

    revert

--- user_signup.php
+++ user_signup.php
@@ -9,29 +9,29 @@ $password = password_hash($_POST['password'], PASSWORD_DEFAULT); // Hashing the 
 $role = $_POST["role"] == "agent" ? "agent" : "client"; 
 
 
-$stmt = $conn->prepare("SELECT iduser, email FROM users WHERE email = ?");
-$stmt->bind_param('s', $email);
-$stmt->execute();
-$result = $stmt->get_result();
+$query = "SELECT iduser, email FROM users WHERE email = '$email'"; 
+$result = mysqli_query($conn, $query);
 
 // Check if query failed
-if ($result === false){
+if ($result == false){
     header("Location: query_error.html");
     die("Query failed: " . mysqli_error($conn));
 }
 
-// Check if query failed
-if($result->num_rows > 0){
+// Check if user already exists
+if(mysqli_num_rows($result) > 0){
     header("Location: user_exists.html"); 
-    die("User already exists");
+   die("User already exists");
 } else {
+
+    // Insert user into database
     //user table strucutre = iduser, email, password, role, registered
-    $stmt = $conn->prepare("INSERT INTO `users` (`email`, `password`, `role`, `registered`) VALUES (?, ?, ?, current_timestamp())");
-    $stmt->bind_param('sss', $email, $password, $role);
-    $stmt->execute();
+    $query = "INSERT INTO `users` (`email`, `password`, `role`, `registered`) VALUES ('$email', '$password', '$role', current_timestamp())";
+
+    $result = mysqli_query($conn, $query); 
 
     // Check if query failed
-    if($stmt === false){
+    if(!$result){
         header("Location: query_error.html");
         die("Query failed: " . mysqli_error($conn));
     }
@@ -46,22 +46,21 @@ if($result->num_rows > 0){
         $agent_country = $_POST['agent_country'];
         $agent_website = $_POST['agent_website'];
         //agent table structure = user_id, agent_firstname, agent_lastname, agent_tel, agent_city, agent_country, agent_website
-        $stmt = $conn->prepare("INSERT INTO `agent` (`user_id`, `agent_firstname`, `agent_lastname`, `agent_tel`, `agent_city`, `agent_country`, `agent_website`) VALUES (?, ?, ?, ?, ?, ?, ?)");
-        $stmt->bind_param('issssss', $user_id, $firstname, $lastname, $agent_tel, $agent_city, $agent_country, $agent_website);
+        $query = "INSERT INTO `agent` (`user_id`, `agent_firstname`, `agent_lastname`, `agent_tel`, `agent_city`, `agent_country`, `agent_website`) VALUES ('$user_id', '$firstname', '$lastname', '$agent_tel', '$agent_city', '$agent_country', '$agent_website')";
     } else {
         //client table structure = user_id, client_firstname, client_lastname
-        $stmt = $conn->prepare("INSERT INTO `client` (`user_id`, `client_firstname`, `client_lastname`) VALUES (?, ?, ?)");
-        $stmt->bind_param('iss', $user_id, $firstname, $lastname);
+        $query = "INSERT INTO `client` (`user_id`, `client_firstname`, `client_lastname`) VALUES ('$user_id', '$firstname', '$lastname')";
     }
     
-    $stmt->execute();
+    $result2 = mysqli_query($conn, $query);
 
     // Check if query failed
-    if($stmt === false){
+    if(!$result2){
         header("Location: query_error.html");
         die("Query failed: " . mysqli_error($conn));
     }
 
+    // Start session
     $_SESSION["user_id"] = $user_id;
     $_SESSION["role"] = $role;
 
@@ -71,4 +70,5 @@ if($result->num_rows > 0){
         header("Location: dashboard-agent.html");
     }
 }
+
 ?>
