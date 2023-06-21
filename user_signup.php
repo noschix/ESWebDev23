892a91fda9f3e302419a6e000d8d28e0a5c3de22
Noah Raissi <coding@noahraissi.com>
2023-06-21 13:55:54 +0200

    sql safe

--- user_signup.php
+++ user_signup.php
@@ -23,53 +23,47 @@ if ($result == false){
 if(mysqli_num_rows($result) > 0){
     header("Location: user_exists.html"); 
    die("User already exists");
-} else {
+} else else {
 
     // Insert user into database
     //user table strucutre = iduser, email, password, role, registered
-    $query = "INSERT INTO `users` (`uuid`,`email`, `password`, `role`, `registered`) VALUES (NULL,`$email`, `$password`, `$role`, current_timestamp())";
+    $stmt = $conn->prepare("INSERT INTO `users` (`uuid`,`email`, `password`, `role`, `registered`) VALUES (NULL, ?, ?, ?, current_timestamp())");
+    $stmt->bind_param("sss", $email, $password, $role);
 
-    $result = mysqli_query($conn, $query); 
+    if($stmt->execute()) {
+        // Get user id
+        $user_id = $conn->insert_id;
 
-    // Check if query failed
-    if(!$result){
-        header("Location: query_error.html");
-        die("Query failed: " . mysqli_error($conn));
-    }
+        // Insert user into agent or client table
+        if($role == "agent"){
+            $agent_about = $_POST['agent_about'];
+            $agent_city = $_POST['agent_city'];
+            $agent_country = $_POST['agent_country'];
+            $agent_website = $_POST['agent_website'];
+            $agent_exp = $_POST['agent_exp'];
 
-    // Get user id
-    $user_id = $conn->insert_id;
+            $stmt2 = $conn->prepare("INSERT INTO `agents` (`agent_id`,`user_id`, `agent_firstname`, `agent_lastname`,`agent_exp`,`agent_about`, `agent_city`, `agent_country`, `agent_website`) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?)");
+            $stmt2->bind_param("isssssss", $user_id, $firstname, $lastname, $agent_exp, $agent_about, $agent_city, $agent_country, $agent_website);
+            $stmt2->execute();
+        } else {
+            $stmt2 = $conn->prepare("INSERT INTO `clients` (`client_id`,`user_id`, `client_firstname`, `client_lastname`) VALUES (NULL, ?, ?, ?)");
+            $stmt2->bind_param("iss", $user_id, $firstname, $lastname);
+            $stmt2->execute();
+        }
 
-    // Insert user into agent or client table
-    if($role == "agent"){
-        $agent_about = $_POST['agent_about'];
-        $agent_city = $_POST['agent_city'];
-        $agent_country = $_POST['agent_country'];
-        $agent_website = $_POST['agent_website'];
-        $agent_exp = $_POST['agent_exp'];
-        //agent table structure = user_id, agent_firstname, agent_lastname, agent_about, agent_city, agent_country, agent_website
-        $query = "INSERT INTO `agents` (`agent_id`,`user_id`, `agent_firstname`, `agent_lastname`,`agent_exp`,`agent_about`, `agent_city`, `agent_country`, `agent_website`) VALUES (NULL,`$user_id`, `$firstname`, `$lastname`,`$agent_exp`,`$agent_about`, `$agent_city`, `$agent_country`, `$agent_website`)";
+        // Start session
+        $_SESSION["user_id"] = $user_id;
+        $_SESSION["role"] = $role;
+
+        if($role == "client"){
+            header("Location: dashboard-client.html");
+        } else {
+            header("Location: dashboard-agent.html");
+        }
+
     } else {
-        //client table structure = user_id, client_firstname, client_lastname
-        $query = "INSERT INTO `clients` (`client_id`,`user_id`, `client_firstname`, `client_lastname`) VALUES (NULL,`$user_id`, `$firstname`, `$lastname`)";
-    }
-    
-    $result2 = mysqli_query($conn, $query);
-
-    // Check if query failed
-    if(!$result2){
         header("Location: query_error.html");
-        die("Query failed: " . mysqli_error($conn));
-    }
-
-    // Start session
-    $_SESSION["user_id"] = $user_id;
-    $_SESSION["role"] = $role;
-
-    if($role == "client"){
-        header("Location: dashboard-client.html");
-    } else {
-        header("Location: dashboard-agent.html");
+        die("Query failed: " . $conn->error);
     }
 }
 
