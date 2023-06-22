41447694828a50a5ece561d05e278379781ad1bc
Noah Raissi <coding@noahraissi.com>
2023-06-22 18:39:43 +0200

    update

--- deleteUser.php
+++ deleteUser.php
@@ -1,18 +1,17 @@
 <?php
 session_start();
 header('Content-Type: application/json');
-include 'db.php'
 
 if(isset($_SESSION['user_id'])) {
     $userId = $_SESSION['user_id'];
     //establish your db connection here
-    $connNew = $conn;
-
-    if ($connNew->connect_error) {
-        die(json_encode(['error' => 'Connection failed: ' . $connNew->connect_error]));
+    $conn = new mysqli_connect('localhost', 'admin232m', '6WO7hcod~Pn8^umu', 't3_alberto') or die(mysqli_error($mysqli));
+    
+    if ($conn->connect_error) {
+        die(json_encode(['error' => 'Connection failed: ' . $conn->connect_error]));
     }
 
-    $stmt = $connNew->prepare("DELETE FROM users WHERE uuid = ?");
+    $stmt = $conn->prepare("DELETE FROM users WHERE uuid = ?");
     $stmt->bind_param("s", $userId);
 
     if($stmt->execute()){
