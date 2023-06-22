b4f62b57b1fa65814d412f8c8d5f7fd7a42cb3a9
Noah Raissi <coding@noahraissi.com>
2023-06-22 18:43:39 +0200

    refractor

--- deleteUser.php
+++ deleteUser.php
@@ -1,17 +1,18 @@
 <?php
-session_start();
+
+include('db.php'); 
 header('Content-Type: application/json');
 
 if(isset($_SESSION['user_id'])) {
     $userId = $_SESSION['user_id'];
     //establish your db connection here
-    $conn = new mysqli_connect('localhost', 'admin232m', '6WO7hcod~Pn8^umu', 't3_alberto') or die(mysqli_error($mysqli));
+    $connNew = $conn;
     
-    if ($conn->connect_error) {
-        die(json_encode(['error' => 'Connection failed: ' . $conn->connect_error]));
+    if ($connNew->connect_error) {
+        die(json_encode(['error' => 'Connection failed: ' . $connNew->connect_error]));
     }
 
-    $stmt = $conn->prepare("DELETE FROM users WHERE uuid = ?");
+    $stmt = $connNew->prepare("DELETE FROM users WHERE uuid = ?");
     $stmt->bind_param("s", $userId);
 
     if($stmt->execute()){
